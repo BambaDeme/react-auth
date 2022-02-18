26357a3a7bd1c605e32f827c6324c15d339ed392
bambademe <cheikhbambademe@gmail.com>
2022-02-18 15:26:42 -0800

    sign up ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import SignupModal from "./components/SignupModal";
 
 import Home from "./pages/Home";
 function App() {
   return (
     <>
       <Navbar />
+      <SignupModal />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
