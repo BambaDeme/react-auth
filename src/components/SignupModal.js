40324e57b19f441153e3457d8fa3d31405cb48a5
bambademe <cheikhbambademe@gmail.com>
2022-02-19 02:19:34 -0800

    firebase sign up, server validation

--- src/components/SignupModal.js
+++ src/components/SignupModal.js
@@ -1,16 +1,62 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef, useState } from "react";
 
 import { UserContext } from "../context/userContext";
 const SignupModal = () => {
-  const { modalState, toggleModals } = useContext(UserContext);
-  console.log(modalState, toggleModals);
+  const { modalState, toggleModals, signUp } = useContext(UserContext);
+
+  const inputs = useRef([]);
+  const addInputs = (el) => {
+    if (el && !inputs.current.includes(el)) {
+      inputs.current.push(el);
+    }
+  };
+
+  const formRef = useRef();
+
+  const [validation, setValidation] = useState("");
+  const handleForm = async (e) => {
+    e.preventDefault();
+    if (
+      inputs.current[1].value.length < 6 ||
+      inputs.current[2].value.length < 6
+    ) {
+      setValidation("6 character minimum for the password");
+      return;
+    }
+    if (inputs.current[1].value !== inputs.current[2].value) {
+      setValidation("Passwords do not match");
+      return;
+    }
+
+    try {
+      const cred = await signUp(
+        inputs.current[0].value,
+        inputs.current[1].value
+      );
+      formRef.current.reset();
+      setValidation("");
+      console.log(cred);
+    } catch (error) {
+      if (error.code === "auth/invalid-email") {
+        setValidation("Email format invalid");
+      }
+      if (error.code === "auth/email-already-in-use") {
+        setValidation("Email already in use");
+      }
+    }
+  };
+
+  const closeModal = () => {
+    setValidation("");
+    toggleModals("close");
+  };
   return (
     <>
       {modalState.signUpModal && (
         <div className="position-fixed top-0 vw-100 vh-100">
           <div
             className="w-100 h-100 bg-dark bg-opacity-75"
-            onClick={() => toggleModals("close")}
+            onClick={closeModal}
           ></div>
           <div
             className="position-absolute top-50 start-50 translate-middle"
@@ -20,19 +66,21 @@ const SignupModal = () => {
               <div className="modal-content">
                 <div className="modal-header">
                   <h5 className="modal-title">Sign up</h5>
-                  <button
-                    className="btn-close"
-                    onClick={() => toggleModals("close")}
-                  ></button>
+                  <button className="btn-close" onClick={closeModal}></button>
                 </div>
 
                 <div className="modal-body">
-                  <form className="sign-up-form">
+                  <form
+                    className="sign-up-form"
+                    onSubmit={handleForm}
+                    ref={formRef}
+                  >
                     <div className="mb-3">
                       <label htmlFor="signUpEmail" className="form-label">
                         Email adress
                       </label>
                       <input
+                        ref={addInputs}
                         type="email"
                         name="email"
                         required
@@ -46,6 +94,7 @@ const SignupModal = () => {
                         Password
                       </label>
                       <input
+                        ref={addInputs}
                         required
                         type="password"
                         id="signUpPwd"
@@ -59,13 +108,14 @@ const SignupModal = () => {
                         Repeat Password
                       </label>
                       <input
+                        ref={addInputs}
                         required
                         type="password"
                         id="RepeatPwd"
                         className="form-control"
                         name="pwd"
                       />
-                      <p className="text-danger mt-1"></p>
+                      <p className="text-danger mt-1">{validation}</p>
                     </div>
 
                     <button className="btn btn-primary"> Submit</button>
